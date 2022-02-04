fce6d91802d85e12e5cd03b863f85496400144e3
highjun <highjun10170@gmail.com>
2022-02-05 02:51:27 +0900

    Basic Features

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form, Button, Container, Col, Row, ListGroup } from 'react-bootstrap'
-import { Times} from 'react-icons/fa'
 
 const style = {
     h1: { textAlign: 'center', marginTop: '2rem' },
@@ -11,13 +10,31 @@ const style = {
 };
 
 const App = () => {
+    const [todoList, setTODOList] = useState(['TODO List Generation', 'second']);
+    const [input, setInput] = useState('');
+    const deleteTODO = (idx) => {
+        let copy = [...todoList];
+        copy.splice(idx, 1);
+        setTODOList(copy);
+    }
+    const handleInput = (event)=>{
+        // console.log(event);
+        event.preventDefault();
+        setInput('');
+        addTODO();
+    }
+    const addTODO = () => {
+        let copy = [...todoList];
+        copy.push(input);
+        setTODOList(copy);
+    }
     return (
         <Container>
             <h1 style={style.h1}>TODO List</h1>
-            <Form>
+            <Form onSubmit={handleInput}>
                 <Row>
                     <Col className='align-items-stretch'>
-                        <Form.Control placeholder="Enter TODOs" />
+                        <Form.Control placeholder="Enter TODOs" value={input} onChange={({target:{value}}) => setInput(value)}/>
                     </Col>
                     <Col xs='auto'>
                         <Button variant="primary" type="submit">
@@ -28,22 +45,24 @@ const App = () => {
             </Form>
             <hr/>
             <ListGroup>
-                <ListGroup.Item>
+                {todoList.map((val,idx) => (
+                    <ListGroup.Item key= {idx}>
                     <Row>
-                        <Col style={style.list_item}>Cras justo odio</Col>
+                        <Col style={style.list_item}>{val}</Col>
                         <Col xs ='auto'> 
-                            <Button variant="outline-light">
+                            <Button onClick={() => deleteTODO(idx)} variant="outline-light" >
                                 ✔️
                             </Button>
                         </Col>
                         <Col xs ='auto'> 
-                            <Button variant="outline-light">
+                            <Button onClick={() => deleteTODO(idx)} variant="outline-light">
                                 ❌
                             </Button>
                         </Col>
                     </Row>
                 </ListGroup.Item>
-            </ListGroup>
+                ))}
+            </ListGroup>    
         </Container>
     );
 }
